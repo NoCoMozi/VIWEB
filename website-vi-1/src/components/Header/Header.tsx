97974602dad7767df3133710c41564a88db5e9d5
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-02-07 14:28:35 -0600

    fonts, links, styling

--- website-vi-1/src/components/Header/Header.tsx
+++ website-vi-1/src/components/Header/Header.tsx
@@ -2,10 +2,35 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../../public/Images/white-logo.png";
 import "./header.styles.css";
+import Link from "next/link";
+
 const Header = () => {
   return (
     <div className="header-container">
-      <Image src={logo} alt={"Logo"} />
+      <Image className="logo" src={logo} alt={"Logo"} />
+      <div className="links-container">
+        <Link className="link" href={"/home"}>
+          Home
+        </Link>
+        <Link className="link" href={"/about"}>
+          About
+        </Link>
+        <Link className="link" href={"/about"}>
+          Events
+        </Link>
+        <Link className="link" href={"/about"}>
+          Break The Ice
+        </Link>
+        <Link className="link" href={"/about"}>
+          News & Education
+        </Link>
+        <Link className="link" href={"/about"}>
+          Resources
+        </Link>
+        <Link className="link" href={"/about"}>
+          Support
+        </Link>
+      </div>
     </div>
   );
 };
