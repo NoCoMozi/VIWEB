00ca656c31d4783391389b15025f66f31051069a
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-02-10 17:39:52 -0600

    basic button test

--- website-vi-1/src/components/Button/button.test.tsx
+++ website-vi-1/src/components/Button/button.test.tsx
@@ -1,16 +1,26 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "./Button";
+import "@testing-library/jest-dom";
 
 const mockOnClick = jest.fn();
 
-import "@testing-library/jest-dom";
-
 describe("Button Component", () => {
-  it("renders text on button", () => {
+  test("renders text on button", () => {
     render(<Button text={"Submit"} />);
 
     const button = screen.getByRole("button", { name: "Submit" });
 
     expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent("Submit");
+  });
+
+  test("Button calles onClick when called", () => {
+    render(<Button text={"Submit"} onClick={mockOnClick} />);
+
+    const button = screen.getByRole("button", { name: "Submit" });
+
+    fireEvent.click(button);
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+
   });
 });
