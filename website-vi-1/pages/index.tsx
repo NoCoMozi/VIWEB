ea16b98c459491fe367a65ac32775bf4fde42169
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-02-20 13:45:13 -0600

    move to fetching data on server side to avoid loading state

--- website-vi-1/pages/index.tsx
+++ website-vi-1/pages/index.tsx
@@ -1,7 +1,6 @@
 import TopSection from "@/components/TopSection/TopSection";
 import "@/styles/index.styles.scss";
 import "@/styles/variables.scss";
-import { useEffect, useState } from "react";
 
 interface Mission {
   _id: string;
@@ -9,29 +8,27 @@ interface Mission {
   content: string[];
 }
 
-export default function Home() {
-  const [missionData, setMissionData] = useState<Mission[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+export async function getServerSideProps() {
+  try {
+    const res = await fetch("http://localhost:3000/api/mission"); // fix for when I have host
+    const missionData = await res.json();
 
-  useEffect(() => {
-    const fetchMissionData = async () => {
-      try {
-        const res = await fetch("/api/mission");
-        const data = await res.json();
-        setMissionData(data);
-      } catch (error) {
-        console.error("Error fetching mission data:", error);
-      } finally {
-        setIsLoading(false);
-      }
+    return {
+      props: {
+        missionData,
+      },
     };
-
-    fetchMissionData();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>; // Show a loading spinner or skeleton
+  } catch (error) {
+    console.error("Error fetching mission data:", error);
+    return {
+      props: {
+        missionData: [],
+      },
+    };
   }
+}
+
+export default function Home({ missionData }: { missionData: Mission[] }) {
   return (
     <div className="page">
       <div className="body-home">
