57be7e0d576b3e9c4e9027a4838eb45b3f283865
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-02-21 15:18:35 -0600

    fixing favicon

---
+++ app/.next/static/chunks/pages/index.js
@@ -0,0 +1,9 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/node_modules_next_71458a._.js",
+  "static/chunks/node_modules_react-dom_82bb97._.js",
+  "static/chunks/node_modules_1b7400._.js",
+  "static/chunks/[root of the server]__3c9dba._.js",
+  "static/chunks/src_styles_dd3005._.css",
+  "static/chunks/pages_index_5771e1._.js",
+  "static/chunks/pages_index_791e5a._.js"
+])
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -8,7 +8,21 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" /> {/* Add this line */}
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="VI" />
+        <link rel="manifest" href="/site.webmanifest" />{" "}
       </Head>
       <Header />
       <main>
