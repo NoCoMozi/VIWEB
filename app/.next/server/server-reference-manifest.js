29d425f106a351234936dc8c18bbde0962f07744
Mary Kilduff <marykilduff@Marys-MacBook-Pro-4.local>
2025-02-21 15:19:56 -0600

    please lord let this fix it

--- app/.next/server/server-reference-manifest.js
+++ app/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"E4zNoBnwHTJSyx+SoDHcAAk+KTfDrstBEiszVaEv0TY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
